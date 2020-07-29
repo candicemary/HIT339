c73e2c36955381975869a6a52acad6675fb14f8c
candicemary <candice.carsia@outlook.com>
2020-07-29 18:39:05 +0930

    Adding model

---
+++ Week1 HelloWorldMVC/Week1HelloWorldMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week1HelloWorldMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
