207391598b22a9a6d2eb84cccc9d0e4610392f1a
candicemary <candice.carsia@outlook.com>
2020-08-04 17:20:13 +0930

    Creating Movie Id name director release date and category

--- Wee2MVC_Web_APP/MVCWebAPP/MVCWebAPP/00data/EFModels.cs
+++ Wee2MVC_Web_APP/MVCWebAPP/MVCWebAPP/00data/EFModels.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MVCWebAPP._00data
 {
-    public class EFModels
+    public class Movie
+
     {
+        public int MovieID { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public string Release_Dates { get; set; }
+        public string Email { get; set; }
+        public string language { get; set; }
+        public string category { get; set; }
     }
+   
+
+
+
 }
+
