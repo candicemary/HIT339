3af2935dca6ba171cc28377d02d4e5afe853ec98
candicemary <candice.carsia@outlook.com>
2020-08-12 19:04:55 +0930

    Creating angelar web project

---
+++ week4/Week4TUT/Week4TUT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week4TUT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ week4/week4Angelar/week4Angelar/Controllers/StudentsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using week4Angelar.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace week4Angelar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+
+        {
+            List<Student> oStudents = new List<Student>(){
+                new Student(){Id = 1,Name=" Candice",Roll = 1000},
+                new Student(){Id = 2,Name=" Rose",Roll = 1001},
+                new Student(){Id = 3,Name=" LILI",Roll = 1002}
+          
+            };
+            return oStudents;
+
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
